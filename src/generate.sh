2b2c2578675bc0d67fde5509a08449961f5ad709
Sengorius <mail@skript-manufaktur.de>
2024-03-28 20:29:46 +0100

    added a function to create another certificate, handling multilevel subdomains

--- src/generate.sh
+++ src/generate.sh
@@ -11,10 +11,27 @@ function generate_openssl_certs() {
     local RSA_ORG_NAME=$6
     local RSA_ORGUNIT_NAME=$7
     local RSA_EMAIL=$8
+    local SUBDOMAIN=$9
 
     # create complex variables
-    local DOMAIN_ALT_NAMES="DNS:docker.test,DNS:*.docker.test,DNS:localhost,DNS:127.0.0.1,DNS:0:0:0:0:0:0:0:1"
-    local CERT_SUBJECT="/C=$RSA_COUNTRY_NAME/ST=$RSA_STATE_NAME/L=$RSA_LOCALITY_NAME/O=$RSA_ORG_NAME/OU=$RSA_ORGUNIT_NAME/emailAddress=$RSA_EMAIL/CN=docker.test"
+    local CN
+    local DOMAIN_ALT_NAMES
+    local KEYFILE
+    local ROOTCA
+
+    if [[ -n "$SUBDOMAIN" ]]; then
+        CN="$SUBDOMAIN.docker.test"
+        DOMAIN_ALT_NAMES="DNS:$SUBDOMAIN.docker.test,DNS:*.$SUBDOMAIN.docker.test"
+        KEYFILE="$CERTS_PATH/$SUBDOMAIN.docker.test"
+        ROOTCA="$CERTS_PATH/$SUBDOMAIN.rootCA"
+    else
+        CN="docker.test"
+        DOMAIN_ALT_NAMES="DNS:docker.test,DNS:*.docker.test,DNS:localhost,DNS:127.0.0.1,DNS:0:0:0:0:0:0:0:1"
+        KEYFILE="$CERTS_PATH/docker.test"
+        ROOTCA="$CERTS_PATH/rootCA"
+    fi
+
+    local CERT_SUBJECT="/C=$RSA_COUNTRY_NAME/ST=$RSA_STATE_NAME/L=$RSA_LOCALITY_NAME/O=$RSA_ORG_NAME/OU=$RSA_ORGUNIT_NAME/emailAddress=$RSA_EMAIL/CN=$CN"
 
     if ! command -v openssl; then
         print_warning "Openssl seems not to be installed on this machine. But it is necessary to generate keys..." 1
@@ -26,28 +43,28 @@ function generate_openssl_certs() {
     fi
 
     # create a root key and rootCA for docker.test
-    ${RSA_COMMAND} genrsa -out "$CERTS_PATH/rootCA.key" "$RSA_KEY_LENGTH"
+    ${RSA_COMMAND} genrsa -out "$ROOTCA.key" "$RSA_KEY_LENGTH"
     ${RSA_COMMAND} req -x509 -new -nodes -sha256 \
-        -key "$CERTS_PATH/rootCA.key" \
+        -key "$ROOTCA.key" \
         -subj "$CERT_SUBJECT" \
         -days "$RSA_VALID_DAYS" \
-        -out "$CERTS_PATH/rootCA.crt"
+        -out "$ROOTCA.crt"
 
     # create a certificate for docker.test
-    ${RSA_COMMAND} genrsa -out "$CERTS_PATH/docker.test.key" "$RSA_KEY_LENGTH"
+    ${RSA_COMMAND} genrsa -out "$KEYFILE.key" "$RSA_KEY_LENGTH"
     ${RSA_COMMAND} req -new -sha256 \
-        -key "$CERTS_PATH/docker.test.key" \
+        -key "$KEYFILE.key" \
         -subj "$CERT_SUBJECT" \
         -reqexts SAN \
         -config <(cat /etc/ssl/openssl.cnf <(printf "\n[SAN]\nsubjectAltName=$DOMAIN_ALT_NAMES")) \
-        -out "$CERTS_PATH/docker.test.csr"
+        -out "$KEYFILE.csr"
 
     # last step: sign the domain certificate with the rootCA
     ${RSA_COMMAND} x509 -req -sha256 -CAcreateserial \
-        -in "$CERTS_PATH/docker.test.csr" \
-        -CA "$CERTS_PATH/rootCA.crt" \
-        -CAkey "$CERTS_PATH/rootCA.key" \
-        -out "$CERTS_PATH/docker.test.crt" \
+        -in "$KEYFILE.csr" \
+        -CA "$ROOTCA.crt" \
+        -CAkey "$ROOTCA.key" \
+        -out "$KEYFILE.crt" \
         -days "$RSA_VALID_DAYS" \
         -extfile <(printf "subjectAltName=$DOMAIN_ALT_NAMES")
 }
